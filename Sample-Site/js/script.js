1e81b2d3b30ebbfb1353c965edf628b02395a3a8
Gabriela Guedez Hamerlok <gaby.guedez@gmail.com>
2018-05-09 12:41:43 +0000

    completed the challenge but the buttons were looking odd. Commented my code and copied pasted the solution, the bottons also look odd in the solution so perhaps this is the way they're supposed to look like. Decided to leave it like this for the time being

--- Sample-Site/js/script.js
+++ Sample-Site/js/script.js
@@ -1,17 +1,52 @@
 $(document).ready(function() {
 
-	$("#button1").mouseenter(function(){
-		$("#button1").removeClass("makeRed").addClass("border");
-	});
-	$("#button1").mouseleave(function(){
-		$("#button1").removeClass("border").addClass("makeRed");
-	});
+	// $("#button1").mouseenter(function(){
+	// 	$("#button1").removeClass("makeRed").addClass("border");
+	// });
+	// $("#button1").mouseleave(function(){
+	// 	$("#button1").removeClass("border").addClass("makeRed");
+	// });
 	
-	$("#button1").click(function(){
-	 $("#para1").slideToggle("slow");
-	});
+	// // $("#button1").click(function(){
+	// //  $("#para1").slideToggle("slow");
+	// // });
 	
-		$("#button2").click(function(){
-	 $("#para2").hide().show().fadeIn().fadeOut();
-	});
+	// // 	$("#button2").click(function(){
+	// //  $("#para2").hide().show().fadeIn().fadeOut();
+	// // });
+	
+	// // The above code was commented because it doesn't fully sovle the challenge. The paragraph dissapears but then comes back
+	
+	// //hides/shows  paragraphs when either button is clicked
+ // 	$("button").click(function(){
+ // 	 $("p").hide(2000).show(2000);
+ // 	});
+ // 		//fadeIn and fadoeOut on paragraphs when either button is clicked
+ // 	$("button").click(function(){
+ // 	 $("p").fadeIn().fadeOut();
+ // 	});
+ 	
+ 	//revoves claas makeRed, adds class makeBorder on mouseenter
+ 	$("button").mouseenter(function(){
+ 	 $(this).removeClass("makeRed").addClass("makeBorder");
+
+ 	});
+ 	//reverses above on mouseleave
+ 	$("button").mouseleave(function(){
+ 	 $("button").removeClass("makeBorder").addClass("makeRed");
+
+ 	});
+ 	//toggles paragraphs when either button is clicked
+ 	$("button").click(function(){
+ 	 $("p").slideToggle(2000);
+ 	});
+
+ 	//hides/shows  paragraphs when either button is clicked
+ 	$("button").click(function(){
+ 	 $("p").hide(2000).show(2000);
+ 	});
+ 		//fadeIn and fadoeOut on paragraphs when either button is clicked
+ 	$("button").click(function(){
+ 	 $("p").fadeIn().fadeOut();
+ 	});
 }); 
